0e86b1da54b90f5fa34571d99f9eebb4b9c5d0fe
abgill <andrewg95@gmail.com>
2019-10-05 15:52:34 -0600

    added threading

--- controler.py
+++ controler.py
@@ -2,6 +2,8 @@ import os
 import struct
 from ControllerState import ControllerState
 from time import sleep
+from thread import start_new_thread
+import threading
 
 from pprint import pprint
 
@@ -11,8 +13,10 @@ dev = os.open("/dev/input/js0", os.O_RDONLY)
 print(dev)
 
 controller_state = ControllerState()
+lock = threading.Lock()
 
 def unpack_button(button_val, button):
+    lock.acquire()
     if(button == b'\x00'):
         controller_state.a = button_val
     elif(button == b'\x01'):
@@ -21,11 +25,13 @@ def unpack_button(button_val, button):
         controller_state.y = button_val
     elif(button == b'\x03'):
         controller_state.x = button_val
+    lock.release()
 
 def get_norm_axis_val(axis_val):
     return axis_val / 32767.0
 
 def unpack_axis(axis_val, axis):
+    lock.acquire()
     if axis == b'\x04':
         controller_state.lt = get_norm_axis_val(axis_val)
     elif axis == b'\x05':
@@ -38,19 +44,27 @@ def unpack_axis(axis_val, axis):
         controller_state.rs_x = get_norm_axis_val(axis_val)
     elif axis == b'\x03':
         controller_state.rs_y = get_norm_axis_val(axis_val)
+    lock.release()
         
     
     pass
 
-while True:
-    evnt_str = os.read(dev, JS_EVENT_SIZE)
 
-    event = struct.unpack("Ihcc", evnt_str)
+def start_js_listner():
+    while True:
+        evnt_str = os.read(dev, JS_EVENT_SIZE)
 
-    if(event[2] == b'\x01'):
-        unpack_button(event[1], event[3])
-        # pprint(vars(controller_state))
-    elif(event[2] == b'\x02'):
-        unpack_axis(event[1], event[3])
-        pprint(vars(controller_state))
+        event = struct.unpack("Ihcc", evnt_str)
+
+        if(event[2] == b'\x01'):
+            unpack_button(event[1], event[3])
+            # pprint(vars(controller_state))
+        elif(event[2] == b'\x02'):
+            unpack_axis(event[1], event[3])
+            pprint(vars(controller_state))
+
+start_new_thread(start_js_listner, ())
+
+c = raw_input("Type something to quit.")
+os.close(dev)
 
